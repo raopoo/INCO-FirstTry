db59a0346602099975b5cf30ad20832048d9717e
Pooja Rao <raopooj@gmail.com>
2021-08-21 12:09:24 +1000

    Adding regex for validation. Yet to fix issues

--- form.js
+++ form.js
@@ -6,16 +6,34 @@ let email = document.getElementById('email').value;
 let message = document.getElementById('message').value;
 let modal = document.getElementById("myModal");
 let closeBtn = document.getElementById('closeBtn');
+let form = document.getElementById('contactUS');
 
 //Expressions
-//email
+
 let eValid = /^[A-Za-z0-9\._\-]+@[A-Za-z]+[A-Za-z0-9\-]+[A-Za-z]+(\.[A-Za-z0-9\-]+)+$/;
-//name
 let nValid =/^[A-Za-zÀ-ÖØ-öø-ÿ0-9 \.'\-]+$/;
 
 //Functions
-const formValidation = () => {
+form.setAttribute("nonvalidate", true)
 
+const formValidation = (event) => {
+   event.preventDefault();
+
+  if(validInput(nValid, firstName) && 
+  validInput(nValid, lastName) &&
+  validInput(eValid, email)
+  ){
+      console.log(`Name:${firstName} ${lastName}
+      Phone number:${telNum}
+      Email:${email}
+      Message:${message}`);
+      displayMessage();
+  }else{
+    console.log('Incorrect inputs, Please try again');
+  }
+}
+const validInput = (regex,input) => {
+  return regex.test(input.value);
 }
 
 const displayMessage = () => {
@@ -31,12 +49,12 @@ window.onclick = function(event) {
 //When the user clicks on close button, close the modal
 closeBtn.onclick = function() {
   modal.style.display = "none";
+  form.reset();
 }
 
 
-btn.addEventListener('click', function(){
-    displayMessage();
-    //formValidation();
+btn.addEventListener('submit', function(){
+  formValidation(); 
 });
 
 
